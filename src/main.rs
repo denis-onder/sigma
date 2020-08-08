69cecd96365f2de12bb9639fb56a6e98e7690e05
Denis Onder <denis_onder@protonmail.com>
2020-08-08 14:47:46 +0200

    Unzipping zip archives with files

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,42 @@
-use handlebars::Handlebars;
-use regex::Regex;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::Error;
-
 extern crate handlebars;
-#[macro_use]
-extern crate serde_json;
+extern crate unzip;
+
+// use handlebars::Handlebars;
+use std::fs::File;
+use unzip::Unzipper;
 
 /**
  * CONCEPT:
- * 1. User sends a zipped project with the following structure:
- *    assets
+ * 1. User sends a zipped project with:
+ *    1.1. Assets in assets folder (CSS, JS, etc),
+ *    1.2. Posts in posts folder (Markdown),
+ *    1.3. Templates in the templates folder (Handlebars)
+ *
+ * 2. Unzip the project
+ * 3. Parse Markdown posts
+ * 4. Generate HTML from those posts using the Handlebars engine
+ * 5. Remove the project directory and the zip file
  */
 
-// 1. Read a template HTML file
-fn read_file(path: &str) -> Result<File, Error> {
-    let file = File::open(path)?;
-    return Ok(file);
-}
+// 1. Unzip project
+fn unzip() {
+    let file = File::open("archive.zip");
 
-// 2. Parse the file
-fn parse_html(stringified_file: String) -> Result<Vec<String>, String> {
-    let parsing_regex = Regex::new(r"#output").unwrap();
+    match file {
+        Ok(f) => {
+            let unzip_result = Unzipper::new(f, "./testdir").unzip();
 
-    if parsing_regex.is_match(&stringified_file) {
-        let split = parsing_regex.split(&stringified_file);
-        let res: Vec<String> = split.map(|x| x.to_string()).collect();
-
-        if res.len() >= 2 {
-            return Ok(res);
-        } else {
-            return Err("Invalid template.".to_string());
+            match unzip_result {
+                Ok(_) => println!("Unzipped"),
+                Err(e) => panic!("{}", e),
+            }
         }
+        Err(e) => panic!("{}", e),
     }
-
-    Err("Error".to_string())
 }
 
-// 3. Read JSON file data
-fn parse_json(json_string: String) -> Result<Value, String> {}
+fn main() {
+    println!("Sigma");
 
-fn main() -> Result<(), Error> {
-    let mut html_file = read_file("index.html")?;
-    let mut html_stringified = String::new();
-    html_file.read_to_string(&mut html_stringified)?;
-
-    let res = parse_html(html_stringified);
-
-    match res {
-        Ok(v) => println!("{:?}", v),
-        Err(e) => println!("{}", e),
-    }
-
-    let mut json_data = read_file("mock_data.json")?;
-    let mut json_stringified = String::new();
-    json_data.read_to_string(&mut json_stringified)?;
-
-    println!("{}", json_stringified);
-
-    // let res = parse_json(json_stringified);
-
-    Ok(())
+    unzip();
 }
