1a7e56d2ff42eae6d7244cbf6a2b078bee6ca34b
Denis Onder <denis_onder@protonmail.com>
2020-08-08 13:55:14 +0200

    Handlebars added to project

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use handlebars::Handlebars;
 use regex::Regex;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::Error;
+
+extern crate handlebars;
+#[macro_use]
+extern crate serde_json;
+
+/**
+ * CONCEPT:
+ * 1. User sends a zipped project with the following structure:
+ *    assets
+ */
 
 // 1. Read a template HTML file
 fn read_file(path: &str) -> Result<File, Error> {
@@ -28,11 +39,11 @@ fn parse_html(stringified_file: String) -> Result<Vec<String>, String> {
 }
 
 // 3. Read JSON file data
+fn parse_json(json_string: String) -> Result<Value, String> {}
 
 fn main() -> Result<(), Error> {
     let mut html_file = read_file("index.html")?;
     let mut html_stringified = String::new();
-
     html_file.read_to_string(&mut html_stringified)?;
 
     let res = parse_html(html_stringified);
@@ -41,6 +52,14 @@ fn main() -> Result<(), Error> {
         Ok(v) => println!("{:?}", v),
         Err(e) => println!("{}", e),
     }
+
+    let mut json_data = read_file("mock_data.json")?;
+    let mut json_stringified = String::new();
+    json_data.read_to_string(&mut json_stringified)?;
+
+    println!("{}", json_stringified);
+
+    // let res = parse_json(json_stringified);
 
     Ok(())
 }
